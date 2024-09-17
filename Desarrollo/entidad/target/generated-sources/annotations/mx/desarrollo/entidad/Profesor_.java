5b7f6a9dfafa90a23548a75ed9d6a0c765f69bbb
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2024-09-16 19:36:24 -0700

    entidad

---
+++ Desarrollo/entidad/target/generated-sources/annotations/mx/desarrollo/entidad/Profesor_.java
@@ -0,0 +1,16 @@
+package mx.desarrollo.entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-16T13:48:51")
+@StaticMetamodel(Profesor.class)
+public class Profesor_ { 
+
+    public static volatile SingularAttribute<Profesor, Integer> idProfesor;
+    public static volatile SingularAttribute<Profesor, String> apellido;
+    public static volatile SingularAttribute<Profesor, String> nombre;
+    public static volatile SingularAttribute<Profesor, Integer> rfc;
+
+}
